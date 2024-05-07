ef89f91697c0fbfdf4638921efec8ab08785cbfb
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-05-06 22:25:47 -0300

    Update imc.js
    Atualização para que o número decimal do IMC fique apenas com 2 casas.

--- imc.js
+++ imc.js
@@ -1,4 +1,6 @@
 let peso = parseFloat(prompt("Informe o seu peso: "));
 let altura = parseFloat(prompt("Informe a sua altura"));
 
-alert("O seu imc é: " + (peso / (altura * altura)));
+let imc = parseFloat(peso / (altura * altura));
+
+alert("O seu imc é: " + imc.toFixed(2));
